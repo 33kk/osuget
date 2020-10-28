ffbcb3b6274012008c85defed15519dba37c2bbd
Marko <goncarmarko@gmail.com>
2020-10-28 23:10:29 +0200

    why does this work

--- osuget.user.js
+++ osuget.user.js
@@ -11,53 +11,28 @@
 const idRegex = /https:\/\/osu\.ppy\.sh\/beatmapsets\/([0-9]+)/;
 
 function getBeatmapSetId(str) {
-  return str.match(idRegex)[1];
+	return str.match(idRegex)[1];
 }
 
 function click(e) {
-  let t;
-  if (e.target) {
-    if (e.target.href) {
-      t = e.target;
-    }
-    else if (e.target.parentElement && e.target.parentElement.href) {
-      t = e.target.parentElement;
-    }
-    else if (e.target.parentElement.parentElement && e.target.parentElement.parentElement.href) {
-      t = e.target.parentElement.parentElement;
-    }
-  }
-  if (t) {
-    if (t.classList && t.classList.contains("js-beatmapset-download-link") && idRegex.test(t.href)) {
-      console.log("osuget: click");
-      t.href = "osuget:" + getBeatmapSetId(t.href);
-    }
-    else if (t.classList && t.classList.contains("btn-osu-big--beatmapset-header") && t.href === "https://osu.ppy.sh/home/support") {
-      t.href = "osuget:" + getBeatmapSetId(window.location.href);
-    }
-  }
+	let t;
+	if (e.target) {
+		if (e.target.href) {
+			t = e.target;
+		} else if (e.target.parentElement && e.target.parentElement.href) {
+			t = e.target.parentElement;
+		} else if (e.target.parentElement.parentElement && e.target.parentElement.parentElement.href) {
+			t = e.target.parentElement.parentElement;
+		}
+	}
+	if (t) {
+		if (t.classList && t.classList.contains("js-beatmapset-download-link") && idRegex.test(t.href)) {
+			console.log("osuget: click");
+			t.href = "osuget:" + getBeatmapSetId(t.href);
+		} else if (t.classList && t.classList.contains("btn-osu-big--beatmapset-header") && t.href === "https://osu.ppy.sh/home/support") {
+			t.href = "osuget:" + getBeatmapSetId(window.location.href);
+		}
+	}
 }
 
-const _wr = function(type) {
-    const orig = history[type];
-    return function() {
-        const rv = orig.apply(this, arguments);
-        let e = new Event(type);
-        e.arguments = arguments;
-        window.dispatchEvent(e);
-        return rv;
-    };
-};
-history.pushState = _wr('pushState'), history.replaceState = _wr('replaceState');
-
-window.addEventListener('popstate', (event) => {
-  console.log("osuget: popstate");
-  setTimeout(() => document.body.addEventListener("click", click), 1500)
-});
-
-window.addEventListener('replaceState', (event) => {
-  console.log("osuget: replaceState");
-  setTimeout(() => document.body.addEventListener("click", click), 1500)
-});
-
-document.body.addEventListener("click", click);
+document.querySelector("html").addEventListener("click", click);
